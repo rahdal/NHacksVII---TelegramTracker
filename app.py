e2a986a65098f95a1e0fb3b1fac14017e6405176
rahdal <rdalvi2014@gmail.com>
2022-05-21 20:35:51 -0400

    Made website look good

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import json
 import plotly
 import plotly.express as px
 import plotly.graph_objects as go
+import plotly.io as pio
+
+pio.templates.default = 'plotly_dark'
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -12,8 +15,9 @@ app.static_folder = 'static'
 def index():
     
     CommonPairings = pd.read_csv('CommonPairings.csv')
-    pairings = px.bar(CommonPairings, x = 'Pair', y = 'Messages')
+    pairings = px.bar(CommonPairings, x = 'Messages', y = 'Pair', orientation = 'h', width=1600, height=400)
     
     graphJSON = json.dumps(pairings, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON1 = json.dumps(pairings, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return render_template('index.html', graphJSON = graphJSON)
+    return render_template('index.html', graphJSON = graphJSON, graphJSON1 = graphJSON1)
